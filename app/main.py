05171bd201a7e154a8ddffe53604a0cc1348f342
Tiana Mahery <tiana.mahery526@example.com>
2026-07-14 14:59:25 +0300

    Version 1.0.2

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.get("/", response_class=HTMLResponse)
 def home():
-    version = "v1.0.1"
+    version = "v1.0.2"
     heure = datetime.datetime.now().strftime("%H:%M:%S")
     date = datetime.datetime.now().strftime("%d/%m/%Y")
 
